3da8e7e01c8816b2088fcff415bad4cc376ae972
jakobhelio <jakob.hellrunggg@gmail.com>
2025-03-22 04:31:56 +0100

    Fix base path in vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Telegram-Mini-app/', // Muss mit dem Repository-Namen übereinstimmen
